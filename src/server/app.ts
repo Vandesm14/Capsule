eb51c229277fc3182633064c007c3cd499fe4b25
Shane Vandegrift <vandesm14@gmail.com>
2021-12-13 23:41:47 -0500

    add biGrams to server

--- src/server/app.ts
+++ src/server/app.ts
@@ -8,7 +8,6 @@ const obsidian = process.env.OBSIDIAN;
 
 function recursiveReadDir(dir: string, map: Map<string, Set<string>>) {
   const files = readdirSync(dir);
-  let total = 0;
   for (const file of files) {
     const fullPath = path.join(dir, file);
     if (statSync(fullPath).isDirectory()) {
@@ -16,25 +15,20 @@ function recursiveReadDir(dir: string, map: Map<string, Set<string>>) {
         recursiveReadDir(fullPath, map);
       }
     } else if (file.endsWith('.md')) {
-      total++;
       const content = readFileSync(fullPath, 'utf8');
-      const tags = content.match(/#[a-zA-Z0-9]+/g) ?? [];
+      const tags = content.match(/(?<![\w\d])#[a-zA-Z0-9\-_\/]+/g) ?? [];
       const links = content.match(/(?<!\!)\[\[(.*?)\]\]/g) ?? [];
-
       if (tags.length > 0 || links.length > 0) {
         const hash = fullPath.split('/').join('');
         map.set(hash, new Set([...tags, ...links]));
       }
-
       console.log(`Found ${tags ? tags.length : 0} tags in ${fullPath}`);
     }
   }
-
   return map;
 }
 
 const data = recursiveReadDir(obsidian, new Map());
-
 const count = new Map<string, number>();
 for (const [key, value] of data.entries()) {
   for (const tag of value) {
@@ -49,3 +43,22 @@ const sorted = new Map([...count.entries()].sort((a, b) => b[1] - a[1]));
 
 console.log(data);
 console.log(sorted);
+
+const nGrams = new Map();
+
+for (const [key, value] of data.entries()) {
+  const tags = [...value];
+  for (let i = 0; i < tags.length - 1; i++) {
+    if (tags[i].startsWith('[[') || tags[i + 1].startsWith('[[')) continue;
+    const gram = tags[i] + ' ' + tags[i + 1];
+    if (nGrams.has(gram)) {
+      nGrams.set(gram, nGrams.get(gram) + 1);
+    } else {
+      nGrams.set(gram, 1);
+    }
+  }
+}
+
+const sortedGrams = new Map([...nGrams.entries()].sort((a, b) => b[1] - a[1]));
+
+console.log(sortedGrams);
