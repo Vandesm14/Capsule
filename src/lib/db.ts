02e8dc8b51b8cbaff6327d4c08f9a1ef1aaf0767
Shane Vandegrift <vandesm14@gmail.com>
2021-12-14 19:19:17 -0500

    make db better

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,185 +1,166 @@
-type Value = string | number | boolean | null | undefined | object | Value[];
+export type Value = string | Set<string> | string[];
 
-interface db {
-  __testMode: boolean;
-  __testDB: Record<string, string>;
-  __cache: Map<string, Value>;
-  setTestDB: (value: Record<string, Value>) => void;
-  setTestMode: (isDev: boolean) => void;
-  copy: (key: string, newKey: string) => void;
-  rename: (key: string, newKey: string) => void;
-  sort: (key: string, sort: string) => void;
-  type: (key: string) => string;
-  has: (key: string) => boolean;
-  size: () => void;
+class DataSet extends Set<string> {
+  
+}
 
+export class DataStore extends Map<string, Set<string>> {
   // core functions
-  get: (key: string) => Value;
-  set: (key: string, value: Value) => void;
-  del: (key: string) => void;
-  clear: () => void;
-  keys: () => string[];
-  entries: () => [string, Value][];
-}
-
-function get(db: db, key: string): value {
-  let value: string;
-  if (db.__cache.has(key)) {
-    return db.__cache.get(key);
-  } else {
-    if (db.__testMode) {
-      value = db.__testDB[key];
+  create<T extends Value>(key: string, value: T): T {
+    if (this.has(key)) {
+      throw new Error(`Key ${key} already exists`);
     } else {
-      value = localStorage.getItem(key);
-    }
-  }
-
-  try {
-    return JSON.parse(value);
-  } catch (e) {
-    throw new Error(`Error parsing value for key ${key}`);
-  }
-}
-
-function set(db: db, key: string, value: Value) {
-  if (db.__testMode) {
-    db.__testDB[key] = JSON.stringify(value);
-  } else {
-    localStorage.setItem(key, JSON.stringify(value));
-  }
-
-  db.__cache.set(key, value);
-}
-
-function del(db: db, key: string) {
-  if (db.__testMode) {
-    delete db.__testDB[key];
-  } else {
-    localStorage.removeItem(key);
-  }
-
-  db.__cache.delete(key);
-}
-
-function clear(db: db) {
-  if (db.__testMode) {
-    db.__testDB = {};
-  } else {
-    localStorage.clear();
-  }
-
-  db.__cache.clear();
-}
-
-function keys(db: db): string[] {
-  if (db.__testMode) {
-    return Object.keys(db.__testDB);
-  } else {
-    return Object.keys(localStorage);
-  }
-}
-
-function entries(db: db): [string, Value][] {
-  if (db.__testMode) {
-    return Object.entries(db.__testDB);
-  } else {
-    return Object.entries(localStorage).map(el => {
-      if (db.__cache.has(el[0])) {
-        return [el[0], db.__cache.get(el[0])];
+      let _value: Set<string>;
+      if (typeof value === 'string') {
+        _value = new Set([value]);
+      } else if (value instanceof Set) {
+        _value = value;
       } else {
-        return [el[0], JSON.parse(el[1])]
+        _value = new Set(value);
       }
-    });
+      this.set(key, _value);
+      return value;
+    }
   }
-}
-
-const db: db = {
-  __testMode: false,
-  __testDB: {},
-  __cache: new Map(),
-  setTestDB: (value: Record<string, Value>) => {
-    const values: Record<string, string> = {};
-    for (const key in value) {
-      if (value.hasOwnProperty(key)) {
-        values[key] = JSON.stringify(value[key]);
+  read(key: string): Set<string> | null {
+    if (this.has(key)) {
+      return this.get(key);
+    } else {
+      return null;
+    }
+  }
+  update<T extends Value>(key: string, value: T): T {
+    if (this.has(key)) {
+      let _value: Set<string>;
+      if (typeof value === 'string') {
+        _value = new Set([value]);
+      } else if (value instanceof Set) {
+        _value = value;
+      } else {
+        _value = new Set(value);
       }
+      this.set(key, _value);
+      return value;
+    } else {
+      throw new Error(`Key ${key} does not exist`);
     }
-    db.__testDB = values;
-  },
-  setTestMode: (isDev: boolean) => {
-    db.__testMode = isDev;
-  },
-  copy: (key: string, newKey: string) => {
-    if (db.has(key)) {
-      db.set(newKey, db.get(key));
+  }
+  destroy(key: string): void {
+    if (this.has(key)) {
+      this.delete(key);
+    } else {
+      throw new Error(`Key ${key} does not exist`);
     }
-  },
-  rename: (key: string, newKey: string) => {
-    if (db.has(key)) {
-      db.set(newKey, db.get(key));
-      db.del(key);
+  }
+
+  // utility functions
+  rename<K extends string>(oldKey: string, newKey: K): K {
+    if (this.has(oldKey)) {
+      this.set(newKey, this.get(oldKey));
+      this.delete(oldKey);
+      return newKey;
+    } else {
+      throw new Error(`Key ${oldKey} does not exist`);
     }
-  },
-  sort: (key: string, sort: string) => {
-    if (db.has(key)) {
-      const value = db.get(key);
-      if (Array.isArray(value)) {
-        if (sort === 'asc') {
-          return value.sort();
-        } else if (sort === 'desc') {
-          return value.sort().reverse();
+  }
+  duplicate<K extends string>(key: string, newKey: K): K {
+    if (this.has(key)) {
+      this.set(newKey, this.get(key));
+      return newKey;
+    } else {
+      throw new Error(`Key ${key} does not exist`);
+    }
+  }
+
+  // set-level functions
+  push(key: string, value: Value): Set<string> {
+    if (this.has(key)) {
+      let _value: Set<string>;
+      if (typeof value === 'string') {
+        _value = new Set([value]);
+      } else if (value instanceof Set) {
+        _value = value;
+      } else {
+        _value = new Set(value);
+      }
+      const set = this.get(key);
+      for (const item of _value) {
+        set.add(item);
+      }
+      this.set(key, set);
+      return set;
+    } else {
+      throw new Error(`Key ${key} does not exist`);
+    }
+  }
+
+  pop(key: string): Set<string> | null {
+    if (this.has(key)) {
+      const set = this.get(key);
+      if (set.size > 0) {
+        const item = Array.from(set)[set.size - 1];
+        set.delete(item);
+        this.set(key, set);
+        return set;
+      } else {
+        throw new Error(`Key ${key} is empty`);
+      }
+    } else {
+      throw new Error(`Key ${key} does not exist`);
+    }
+  }
+
+  map(key: string, fn: (value: string) => string): Set<string> {
+    if (this.has(key)) {
+      const set = this.get(key);
+      const newSet = new Set<string>();
+      for (const item of set) {
+        newSet.add(fn(item));
+      }
+      this.set(key, newSet);
+      return newSet;
+    } else {
+      throw new Error(`Key ${key} does not exist`);
+    }
+  }
+
+  filter(key: string, fn: (value: string) => boolean): Set<string> {
+    if (this.has(key)) {
+      const set = this.get(key);
+      const newSet = new Set<string>();
+      for (const item of set) {
+        if (fn(item)) {
+          newSet.add(item);
         }
       }
+      this.set(key, newSet);
+      return newSet;
+    } else {
+      throw new Error(`Key ${key} does not exist`);
     }
-    return []
-  },
-  type: (key: string) => {
-    if (db.has(key)) {
-      const value = db.get(key);
-      if (typeof value === 'string') {
-        return 'string';
-      } else if (typeof value === 'number') {
-        return 'number';
-      } else if (typeof value === 'boolean') {
-        return 'boolean';
-      } else if (value === null) {
-        return 'null';
-      } else if (value === undefined) {
-        return 'undefined';
-      } else if (Array.isArray(value)) {
-        return 'array';
-      } else if (typeof value === 'object') {
-        return 'object';
-      }
-    }
-    return 'none';
-  },
-  has: (key: string) => {
-    return db.get(key) !== null;
-  },
-  size: () => {
-    return db.keys().length;
-  },
-
-  // core functions
-  get: (key: string): Value => {
-    return get(db, key);
-  },
-  set: (key: string, value: Value) => {
-    set(db, key, value);
-  },
-  del: (key: string) => {
-    del(db, key);
-  },
-  clear: () => {
-    clear(db);
-  },
-  keys: () => {
-    return keys(db);
-  },
-  entries: () => {
-    return entries(db);
   }
-};
 
-export default db;
+  sort(key: string, fn: (value: string, value2: string) => number): Set<string> {
+    // sort using quick sort
+    if (this.has(key)) {
+      const set = this.get(key);
+      const newSet = new Set<string>();
+      const sort = (arr: string[]): string[] => {
+        if (arr.length <= 1) {
+          return arr;
+        }
+        const pivot = arr[0];
+        const left = arr.slice(1).filter((item) => fn(item, pivot) <= 0);
+        const right = arr.slice(1).filter((item) => fn(item, pivot) > 0);
+        return sort(left).concat([pivot]).concat(sort(right));
+      };
+      for (const item of sort(Array.from(set))) {
+        newSet.add(item);
+      }
+      this.set(key, newSet);
+      return newSet;
+    } else {
+      throw new Error(`Key ${key} does not exist`);
+    }
+  }
+}
--- tests/db.test.ts
+++ tests/db.test.ts
@@ -1,115 +1,119 @@
-// @ts-envorinment jest
+import { DataStore } from '../src/lib/db';
 
-import db from '../src/lib/db';
+let db = new DataStore();
 
 beforeEach(() => {
-  db.clear();
-  expect(db.size()).toBe(0);
-})
-
-it('db starts blank', () => {
-  expect(db.__testDB).toEqual({});
+  db = new DataStore();
+  expect(db.size).toBe(0);
 });
 
-describe('db tests', () => {
-  it('is empty (size)', () => {
-    expect(db.size()).toBe(0);
+describe('DataStore', () => {
+  describe('core functions', () => {
+    it('create with a string', () => {
+      db.create('key', 'value');
+      expect(db.size).toBe(1);
+      expect(db.has('key')).toBe(true);
+      expect(db.read('key')).toEqual(new Set(['value']));
+    });
+
+    it('create with a set', () => {
+      db.create('key', new Set(['foo', 'bar']));
+      expect(db.size).toBe(1);
+      expect(db.has('key')).toBe(true);
+      expect(db.read('key')).toEqual(new Set(['foo', 'bar']));
+    });
+
+    it('create with an array', () => {
+      db.create('key', ['foo', 'bar']);
+      expect(db.size).toBe(1);
+      expect(db.has('key')).toBe(true);
+      expect(db.read('key')).toEqual(new Set(['foo', 'bar']));
+    });
+
+    it('read', () => {
+      db.create('key', 'value');
+      expect(db.read('key')).toEqual(new Set(['value']));
+    });
+
+    it('update', () => {
+      db.create('key', 'value');
+      db.update('key', 'new value');
+      expect(db.read('key')).toEqual(new Set(['new value']));
+    });
+
+    it('destroy', () => {
+      db.create('key', 'value');
+      expect(db.size).toBe(1);
+      expect(db.has('key')).toBe(true);
+      db.destroy('key');
+      expect(db.size).toBe(0);
+      expect(db.has('key')).toBe(false);
+    });
   });
 
-  it('set', () => {
-    db.set('foo', 'bar');
-    expect(db.size()).toBe(1);
+  describe('utility functions', () => {
+    it('rename', () => {
+      db.create('key', 'value');
+      const key = db.rename('key', 'new key');
+      expect(db.size).toBe(1);
+      expect(db.has('key')).toBe(false);
+      expect(db.has('new key')).toBe(true);
+      expect(db.read('new key')).toEqual(new Set(['value']));
+      expect(key).toBe('new key');
+    });
+
+    it('duplicate', () => {
+      db.create('key', 'value');
+      const key = db.duplicate('key', 'new key');
+      expect(db.size).toBe(2);
+      expect(db.has('key')).toBe(true);
+      expect(db.has('new key')).toBe(true);
+      expect(db.read('new key')).toEqual(new Set(['value']));
+      expect(key).toBe('new key');
+    });
   });
 
-  it('set multiple', () => {
-    db.set('foo', 'bar');
-    db.set('bar', 'foo');
-    expect(db.size()).toBe(2);
-  });
+  describe('set-level functions', () => {
+    it('push a string', () => {
+      db.create('key', 'value');
+      db.push('key', 'new value');
+      expect(db.read('key')).toEqual(new Set(['value', 'new value']));
+    });
 
-  it('get', () => {
-    db.set('foo', 'bar');
-    db.set('bar', 'foo');
-    expect(db.get('foo')).toBe('bar');
-    expect(db.get('bar')).toBe('foo');
-  });
+    it('push a set', () => {
+      db.create('key', 'value');
+      db.push('key', new Set(['foo', 'bar']));
+      expect(db.read('key')).toEqual(new Set(['value', 'foo', 'bar']));
+    });
 
-  it('copy', () => {
-    db.set('foo', 'bar');
-    db.copy('foo', 'bar');
-    expect(db.get('foo')).toBe('bar');
-    expect(db.get('bar')).toBe('bar');
-  })
+    it('push an array', () => {
+      db.create('key', 'value');
+      db.push('key', ['foo', 'bar']);
+      expect(db.read('key')).toEqual(new Set(['value', 'foo', 'bar']));
+    });
 
-  it('rename', () => {
-    db.set('foo', 'bar');
-    db.rename('foo', 'bar');
-    expect(db.get('foo')).toBe(null);
-    expect(db.get('bar')).toBe('bar');
-  });
+    it('pop', () => {
+      db.create('key', ['foo', 'bar']);
+      db.pop('key');
+      expect(db.read('key')).toEqual(new Set(['foo']));
+    });
 
-  test.skip('sort', () => {
-    // db.set('foo', 'bar');
-    // db.set('bar', 'foo');
-    // db.sort();
-    // expect(db.keys()).toEqual(['bar', 'foo']);
-  });
+    it('map', () => {
+      db.create('key', ['value', 'foo', 'bar']);
+      const result = db.map('key', (value: string) => value + '!');
+      expect(result).toEqual(new Set(['value!', 'foo!', 'bar!']));
+    });
 
-  it('type', () => {
-    db.set('foo', 'bar');
-    expect(db.type('foo')).toBe('string');
-    db.set('foo', 123);
-    expect(db.type('foo')).toBe('number');
-    db.set('foo', true);
-    expect(db.type('foo')).toBe('boolean');
-    db.set('foo', {});
-    expect(db.type('foo')).toBe('object');
-    db.set('foo', []);
-    expect(db.type('foo')).toBe('array');
-    db.set('foo', null);
-    expect(db.type('foo')).toBe('none');
-  });
+    it('filter', () => {
+      db.create('key', ['value', 'foo', 'bar']);
+      const result = db.filter('key', (value: string) => value.includes('a'));
+      expect(result).toEqual(new Set(['value', 'bar']));
+    });
 
-  it('has', () => {
-    db.set('foo', 'bar');
-    expect(db.has('foo')).toBe(true);
-    expect(db.has('bar')).toBe(false);
-  });
-
-  it('size', () => {
-    db.set('foo', 'bar');
-    expect(db.size()).toBe(1);
-
-    db.set('bar', 'foo');
-    expect(db.size()).toBe(2);
-  });
-
-  it('del', () => {
-    db.set('foo', 'bar');
-    db.set('bar', 'foo');
-    expect(db.size()).toBe(2);
-    db.del('foo');
-    expect(db.size()).toBe(1);
-    expect(db.get('foo')).toBe(null);
-  });
-
-  it('clear', () => {
-    db.set('foo', 'bar');
-    db.set('bar', 'foo');
-    expect(db.size()).toBe(2);
-    db.clear();
-    expect(db.size()).toBe(0);
-  });
-
-  it('keys', () => {
-    db.set('foo', 'bar');
-    db.set('bar', 'foo');
-    expect(db.keys()).toEqual(['foo', 'bar']);
-  });
-
-  it('entries', () => {
-    db.set('foo', 'bar');
-    db.set('bar', 'foo');
-    expect(db.entries()).toEqual([['foo', 'bar'], ['bar', 'foo']]);
+    it('sort', () => {
+      db.create('key', ['value', 'foo', 'bar']);
+      const result = db.sort('key', (value: string, value2: string) => value.localeCompare(value2));
+      expect(result).toEqual(new Set(['bar', 'foo', 'value']));
+    });
   });
 });
