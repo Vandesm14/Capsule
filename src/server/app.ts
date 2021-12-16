8e1466b1c854c22d40a4867d89f34965206c24c4
Shane Vandegrift <vandesm14@gmail.com>
2021-12-15 22:43:17 -0500

    implement nedb into server

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,13 @@
 import { readdirSync, statSync, readFileSync } from 'fs';
+import { DataStore } from 'nedb-promises';
 import path from 'path';
+
+type model = {
+  path: string;
+  tags: string[];
+}
+
+const db = DataStore.create<model>({ filename: 'database.db', autoload: true });
 
 // @ts-ignore
 import dotenv from 'dotenv';
@@ -18,10 +26,10 @@ function recursiveReadDir(dir: string, map: Map<string, Set<string>>) {
       const content = readFileSync(fullPath, 'utf8');
       const tags = content.match(/(?<![\w\d])#[a-zA-Z0-9\-_\/]+/g) ?? [];
       const links = content.match(/(?<!\!)\[\[(.*?)\]\]/g) ?? [];
+      const hash = fullPath.replace(obsidian + '/', '');
       if (tags.length > 0 || links.length > 0) {
-        const hash = fullPath.split('/').join('');
-        map.set(hash, new Set([...tags, ...links]));
       }
+      map.set(hash, new Set([...tags, ...links]));
       console.log(`Found ${tags ? tags.length : 0} tags in ${fullPath}`);
     }
   }
@@ -29,36 +37,48 @@ function recursiveReadDir(dir: string, map: Map<string, Set<string>>) {
 }
 
 const data = recursiveReadDir(obsidian, new Map());
-const count = new Map<string, number>();
-for (const [key, value] of data.entries()) {
-  for (const tag of value) {
-    if (count.has(tag)) {
-      count.set(tag, count.get(tag) + 1);
-    } else {
-      count.set(tag, 1);
-    }
+
+(async () => {
+  console.log(data);
+
+  for (const [key, value] of data.entries()) {
+    await db.insert({ path: key, values: value });
   }
-}
-const sorted = new Map([...count.entries()].sort((a, b) => b[1] - a[1]));
 
-console.log(data);
-console.log(sorted);
+  // sort the db by path and print an array of the paths
+  const sorted = await db.find({}).sort({ path: 1 });
+  console.log(sorted.map(x => x.path));
+})();
 
-const nGrams = new Map();
+// const count = new Map<string, number>();
+// for (const [key, value] of data.entries()) {
+//   for (const tag of value) {
+//     if (count.has(tag)) {
+//       count.set(tag, count.get(tag) + 1);
+//     } else {
+//       count.set(tag, 1);
+//     }
+//   }
+// }
+// const sorted = new Map([...count.entries()].sort((a, b) => b[1] - a[1]));
 
-for (const [key, value] of data.entries()) {
-  const tags = [...value];
-  for (let i = 0; i < tags.length - 1; i++) {
-    if (tags[i].startsWith('[[') || tags[i + 1].startsWith('[[')) continue;
-    const gram = tags[i] + ' ' + tags[i + 1];
-    if (nGrams.has(gram)) {
-      nGrams.set(gram, nGrams.get(gram) + 1);
-    } else {
-      nGrams.set(gram, 1);
-    }
-  }
-}
+// console.log(sorted);
 
-const sortedGrams = new Map([...nGrams.entries()].sort((a, b) => b[1] - a[1]));
+// const nGrams = new Map();
 
-console.log(sortedGrams);
+// for (const [key, value] of data.entries()) {
+//   const tags = [...value];
+//   for (let i = 0; i < tags.length - 1; i++) {
+//     if (tags[i].startsWith('[[') || tags[i + 1].startsWith('[[')) continue;
+//     const gram = tags[i] + ' ' + tags[i + 1];
+//     if (nGrams.has(gram)) {
+//       nGrams.set(gram, nGrams.get(gram) + 1);
+//     } else {
+//       nGrams.set(gram, 1);
+//     }
+//   }
+// }
+
+// const sortedGrams = new Map([...nGrams.entries()].sort((a, b) => b[1] - a[1]));
+
+// console.log(sortedGrams);
